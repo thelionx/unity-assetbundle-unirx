fec9aaff06252457b2cb6efb65ee5eb10a52db98
Fadhil <fadhil@touchten.com>
2016-06-09 16:52:02 +0700

    - added asset bundle example from Unity

---
+++ Assets/AssetBundle-UniRx/Editor/AssetBundlePreferences.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class AssetBundlePreferences
+{
+    #if UNITY_EDITOR
+    static int m_SimulateAssetBundleInEditor = -1;
+    const string kSimulateAssetBundles = "SimulateAssetBundles";
+
+    /// <summary>
+    /// Flag to indicate if we want to simulate assetBundles in Editor without building them actually.
+    /// </summary>
+    public static bool SimulateAssetBundleInEditor
+    {
+        get
+        {
+            if (m_SimulateAssetBundleInEditor == -1)
+                m_SimulateAssetBundleInEditor = EditorPrefs.GetBool(kSimulateAssetBundles, true) ? 1 : 0;
+
+            return m_SimulateAssetBundleInEditor != 0;
+        }
+        set
+        {
+            int newValue = value ? 1 : 0;
+            if (newValue != m_SimulateAssetBundleInEditor)
+            {
+                m_SimulateAssetBundleInEditor = newValue;
+                EditorPrefs.SetBool(kSimulateAssetBundles, value);
+            }
+        }
+    }
+    #endif
+}
